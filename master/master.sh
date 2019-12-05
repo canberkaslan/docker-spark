cb5197415254dbd1be68b7d8b2250d05967da31a
Canberk Aslan <canberk.aslan@n11.com>
2019-12-05 14:00:41 +0300

    certificate

--- master/master.sh
+++ master/master.sh
@@ -11,6 +11,6 @@ mkdir -p $SPARK_MASTER_LOG
 export SPARK_HOME=/spark
 
 ln -sf /dev/stdout $SPARK_MASTER_LOG/spark-master.out
-
+keytool -import -file xpack-ca.crt  -keystore /usr/lib/jvm/java-1.8-openjdk/jre/lib/security/cacerts -storepass "changeit" -noprompt
 cd /spark/bin && /spark/sbin/../bin/spark-class org.apache.spark.deploy.master.Master \
     --ip $SPARK_MASTER_HOST --port $SPARK_MASTER_PORT --webui-port $SPARK_MASTER_WEBUI_PORT >> $SPARK_MASTER_LOG/spark-master.out
